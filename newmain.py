dec605d1ec87542801c8fc46808c72fd78176d06
Nicholas Lee <100244876+nktl-2005@users.noreply.github.com>
2023-11-13 21:33:49 -0500

    Add files via upload

--- newmain.py
+++ newmain.py
@@ -1,6 +1,7 @@
 ### This is the file where we are going to edit the code from the Typing Game. ###
-# Nicholas Lee (NL) #21082020, imported all letters, digits, and punctuation 
+
 from random import choice, randrange
+# Nicholas Lee (NL) #21082020, imported all letters, digits, and punctuation 
 from string import ascii_letters, digits, punctuation
 from turtle import *
 
@@ -15,14 +16,19 @@ def inside(point):
     """Return True if point on screen."""
     return -200 < point.x < 200 and -200 < point.y < 200
 
+def draw_score():
+    global score
+    goto(0, 0)
+    scorekeeper = "Score: " + str(score)
+    write(scorekeeper, align='center', font=('Helvetica', 20, 'normal'))
 
 def draw():
     """Draw letters."""
     clear()
-
+    draw_score()
     for target, letter in zip(targets, letters):
         goto(target.x, target.y)
-        write(letter, align='center', font=('Consolas', 20, 'normal'))
+        write(letter, align='center', font=('Helvetica', 20, 'normal'))
 
     update()
 
@@ -35,6 +41,7 @@ def move():
         target = vector(x, 200)
         targets.append(target)
         letter = choice(ascii_letters)
+
         letters.append(letter)
 
     for target in targets:
@@ -62,6 +69,7 @@ def press(key):
         score -= 1
 
     print('Score:', score)
+
 
 
 setup(420, 420, 370, 0)
